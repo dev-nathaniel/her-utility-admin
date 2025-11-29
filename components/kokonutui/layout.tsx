90cdc6138d363671ef551a104d150ccd44398ffa
Olowodev <nathanielolowofoyeku@gmail.com>
2025-11-29 19:35:19 +0000

    deploy fix

--- components/kokonutui/layout.tsx
+++ components/kokonutui/layout.tsx
@@ -1,8 +1,8 @@
 "use client"
 
 import type { ReactNode } from "react"
-import Sidebar from "./sidebar"
-import TopNav from "./top-nav"
+import Sidebar from "../sidebar"
+import TopNav from "../top-nav"
 import { useTheme } from "next-themes"
 import { useEffect, useState } from "react"
 
